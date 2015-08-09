c00a1d0ecf2bb593976b84bbfba7887e54ec1b3b
KavenC <kavenc@gmail.com>
2015-08-09 14:14:40 +0800

    LinotArgParser class simplification

--- linot/LinotArgParser.py
+++ linot/LinotArgParser.py
@@ -1,6 +1,9 @@
 from __future__ import print_function
 from argparse import SUPPRESS
+
+
 class LinotArgParser:
+
     def __init__(self, subcmd, sub_cmd_parser, default_process):
         ap = sub_cmd_parser.add_parser(subcmd, add_help=False)
         self._arg_parser = ap.add_mutually_exclusive_group()
@@ -8,10 +11,9 @@ class LinotArgParser:
         self._default_process = default_process
         self._arg_list = {}
         self._arg_parser.set_defaults(proc=self._processArgs)
-        self.add_argument('-h', nargs='*', func=self.print_help, help='show command list')
-        self.add_argument('--help', nargs='*', func=self.print_help, help='show command list')
+        self.add_argument('-h', '--help',  action='store_true', func=self.print_help, help='show command list')
 
-    def add_argument(self, arg_str, *args, **kwargs):
+    def add_argument(self, *args, **kwargs):
         if 'dest' in kwargs:
             raise ValueError('"dest" is forbidden for plugin argument')
         if 'func' in kwargs:
@@ -19,15 +21,20 @@ class LinotArgParser:
             kwargs.pop('func')
         else:
             func = None
-        self._arg_parser.add_argument(arg_str, *args, **kwargs)
-        option = arg_str.lstrip(self._arg_parser.prefix_chars)
-        self._arg_list[option] = {}
-        self._arg_list[option]['func'] = func
-        self._arg_list[option]['arg'] = arg_str
-        if 'help' in kwargs:
-            self._arg_list[option]['help'] = kwargs['help']
-        else:
-            self._arg_list[option]['help'] = ''
+        for arg_str in args:
+            # we dont accept positional args
+            if not arg_str.startswith(self._arg_parser.prefix_chars):
+                raise ValueError('supports only optional args starts with ' +
+                                 self._arg_parser.prefix_chars)
+            option = arg_str.lstrip(self._arg_parser.prefix_chars)
+            self._arg_list[option] = {}
+            self._arg_list[option]['func'] = func
+            self._arg_list[option]['arg'] = arg_str
+            if 'help' in kwargs:
+                self._arg_list[option]['help'] = kwargs['help']
+            else:
+                self._arg_list[option]['help'] = ''
+        self._arg_parser.add_argument(*args, **kwargs)
 
     def _processArgs(self, input_args, sender):
         for args in self._arg_list:
@@ -53,5 +60,5 @@ class LinotArgParser:
             help_text = self._arg_list[arg]['help']
             print('{} {}'.format(self._subcmd, self._arg_list[arg]['arg']))
             if help_text != '':
-               print('>> '+help_text)
+                print('>> '+help_text)
             print('-----')
